4b1f15a04759d32a8f90c2060274afe2b53b3b5d
Abdeldjallil Hadji <ncdjilou09@gmail.com>
2022-03-10 01:47:19 +0100

    dockerize env

---
+++ src/infrastructure/repositories/knexRepository.ts
@@ -0,0 +1,23 @@
+import { BaseRepository } from './interfaces/BaseRepository'
+import type { Knex } from 'knex'
+export class KnexRepository<T> implements BaseRepository<T> {
+  constructor(public readonly knex: Knex, public readonly tableName: string) {}
+  get qb(): Knex.QueryBuilder {
+    return this.knex(this.tableName)
+  }
+  find(item: Partial<T>): Promise<T[]> {
+    throw new Error('Method not implemented.')
+  }
+  findOne(id: string | number): Promise<T> {
+    return this.qb.where(id).first()
+  }
+  create(item: T): Promise<T> {
+    throw new Error('Method not implemented.')
+  }
+  update(id: string | number, item: T): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+  delete(id: string | number): Promise<boolean> {
+    throw new Error('Method not implemented.')
+  }
+}
